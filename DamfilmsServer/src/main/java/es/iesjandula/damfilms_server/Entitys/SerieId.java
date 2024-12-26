a3a8c4d8f5be5aeaa7f0f0df55a6522d02f597cf
pabloSerrano19 <psergut234@g.educaand.es>
2024-12-26 17:00:33 +0100

    D-BACK001.3.1: Migrar a tablas con Spring
     -Crea las entidades Temporada, Serie, Genero, Episodio.

---
+++ DamfilmsServer/src/main/java/es/iesjandula/damfilms_server/Entitys/SerieId.java
@@ -0,0 +1,23 @@
+package es.iesjandula.damfilms_server.Entitys;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+public class SerieId implements Serializable
+{
+
+	private static final long serialVersionUID = 1217003096488394025L;
+
+	
+	private String nombre;
+	
+	private Date fechaEstreno;
+}
---
+++ DamfilmsServer/src/main/java/es/iesjandula/damfilms_server/Entitys/Temporada.java
@@ -0,0 +1,30 @@
+package es.iesjandula.damfilms_server.Entitys;
+
+import java.util.List;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="Temporada")
+public class Temporada 
+{
+	@EmbeddedId
+	private TemporadaId temporadaId;
+	
+	@ManyToOne
+	private Serie serie;
+	
+	@OneToMany(mappedBy="temporada")
+	private List<Episodio> episodios;
+
+}
