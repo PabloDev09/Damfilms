e61eac400536bc888cccd19091d41d84f48ad124
PabloDev09 <146177018+PabloDev09@users.noreply.github.com>
2025-01-02 20:01:24 +0100

    D-BACK001.3.3: Migrar a tablas con Spring
     - Eliminacion repositorios rebundantes

--- src/main/java/es/iesjandula/damfilms_server/repositories/EpisodioRepository.java
+++
@@ -1,9 +0,0 @@
-package es.iesjandula.damfilms_server.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.iesjandula.damfilms_server.entities.Episodio;
-
-public interface EpisodioRepository extends JpaRepository<Episodio, Integer>{
-
-}
--- src/main/java/es/iesjandula/damfilms_server/repositories/GeneroRepository.java
+++
@@ -1,9 +0,0 @@
-package es.iesjandula.damfilms_server.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.iesjandula.damfilms_server.entities.Genero;
-
-public interface GeneroRepository extends JpaRepository<Genero, String>{
-
-}
