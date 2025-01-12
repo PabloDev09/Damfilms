676ccc0d8dcdd8581e79910322de55296cfd934e
PabloDev09 <146177018+PabloDev09@users.noreply.github.com>
2025-01-12 13:39:34 +0100

    D-BACK005: Desarrollar endpoints del Swagger
     - Elimina log

---
+++ src/main/java/es/iesjandula/damfilms_server/controller/Controller.java
@@ -0,0 +1,27 @@
+package es.iesjandula.damfilms_server.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import es.iesjandula.damfilms_server.repositories.IRoleRepository;
+import jakarta.annotation.PostConstruct;
+
+@Service
+public class Controller {
+	
+	@Autowired
+	private IRoleRepository iRoleRepository;
+	
+	@PostConstruct
+	public void init() {
+		
+		
+		System.out.println();
+		System.out.println(this.iRoleRepository.entontrarTodosLosRoles());
+		System.out.println();
+
+		
+	}
+
+}
